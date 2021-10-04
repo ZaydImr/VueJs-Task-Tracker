2ccc1c60446877e15e4848f41ae73df505cfe63e
ZAYD ELIMRANI <plutorider47@gmail.com>
2021-10-04 21:53:17 +0100

    Testing

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer : {
         proxy : {
             '^/api': {
-                target: 'http://localhost:5000',
+                target: 'http://localhost:'+process.env.PORT,
                 changeOrigin: true,
                 logLevel: 'debug',
                 pathRewrite: { '^/api': '/' },
